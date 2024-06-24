d96249088195a88c7ef1cd430ae4c0c06fa1bb99
lpascarelli <luca.pascarelli9292@gmail.com>
2024-06-24 17:38:35 +0200

    Start working on getting some movies

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className='h-20 bg-[#0F0F0F] mb-4 flex items-center justify-center'>
+      <h1 className='text-3xl text-white'>Streaming website</h1>
+    </header>
+  );
+}
